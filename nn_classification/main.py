5871cda5b869c9671bc0b859a6f436d8fbf88ccc
say1 <tsydenovsayan@bk.ru>
2021-11-22 07:05:12 +0700

    classification nn: add back propagation

--- nn_classification/main.py
+++ nn_classification/main.py
@@ -2,12 +2,14 @@ import random
 
 import numpy as np
 import random as rd
-np.random.seed(42)
-random.seed(42)
 
 
 def tanh(x):
     return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+
+
+def tanh_derivative(x):
+    return 1 - (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x)) ** 2
 
 
 def softmax(X):
@@ -18,6 +20,10 @@ def softmax(X):
     return expo/expo_sum
 
 
+ALPHA = 0.001
+np.random.seed(42)
+random.seed(42)
+
 x = np.array([[1,1,0],
              [1,0,1],
              [0,1,1],
@@ -27,16 +33,45 @@ x = np.array([[1,1,0],
              [0,1,0],
              [1,1,1],
              [0,0,0]])
+y = np.array([[1,0,0],
+             [1,0,0],
+             [0,1,0],
+             [0,0,1],
+             [0,1,0],
+             [0,0,1],
+             [0,0,1],
+             [1,0,0],
+             [0,1,0]])
 
-
+# Forward prop
 w1 = np.random.sample((3, 4))
 b1 = rd.random()
 
 w2 = np.random.sample((4, 3))
 b2 = rd.random()
 
-a = tanh(np.dot(x, w1) + b1)
-b = softmax(np.dot(a, w2) + b2)
+tensor_1 = np.dot(x, w1) + b1
+hid = tanh(tensor_1)
 
+tensor_2 = np.dot(hid, w2) + b2
+ans = softmax(tensor_2)
 
-print(b)
+e = ans - y
+
+# Back prop
+dE_dAns = ans - y
+
+dE_dw2 = np.dot(hid.T, dE_dAns)
+dE_db2 = dE_dAns
+
+dE_dHid = np.dot(dE_dAns, w2.T) * tanh_derivative(tensor_1)
+dE_dw1 = np.dot(x.T, dE_dHid)
+dE_db1 = dE_dHid
+
+# Correction
+w1 -= ALPHA * dE_dw1
+b1 -= ALPHA * dE_db1
+w2 -= ALPHA * dE_dw2
+b2 -= ALPHA * dE_db2
+
+print(e)
