e68a1ab182e69faf79291052fe18fd189b387934
say1 <tsydenovsayan@bk.ru>
2021-11-29 06:07:35 +0700

    another fix

--- nn_classification/main.py
+++ nn_classification/main.py
@@ -21,8 +21,10 @@ def softmax(X):
 
 
 ALPHA = 0.001
+EPOCHS = 10000
 np.random.seed(42)
 random.seed(42)
+accuracy = []
 
 x = np.array([[1,1,0],
              [1,0,1],
@@ -43,35 +45,41 @@ y = np.array([[1,0,0],
              [1,0,0],
              [0,1,0]])
 
-# Forward prop
-w1 = np.random.sample((3, 4))
-b1 = rd.random()
+for EPOCH in range(EPOCHS):
+    # Forward prop
+    w1 = np.random.sample((3, 4)) * 2 - 1
+    b1 = rd.random() * 2 - 1
 
-w2 = np.random.sample((4, 3))
-b2 = rd.random()
+    w2 = np.random.sample((4, 3)) * 2 - 1
+    b2 = rd.random() * 2 - 1
 
-tensor_1 = np.dot(x, w1) + b1
-hid = tanh(tensor_1)
+    tensor_1 = np.dot(x, w1) + b1
+    hid = tanh(tensor_1)
 
-tensor_2 = np.dot(hid, w2) + b2
-ans = softmax(tensor_2)
+    tensor_2 = np.dot(hid, w2) + b2
+    ans = softmax(tensor_2)
 
-e = ans - y
+    e = ans - y
 
-# Back prop
-dE_dAns = ans - y
+    # Back prop
+    dE_dAns = ans - y
 
-dE_dw2 = np.dot(hid.T, dE_dAns)
-dE_db2 = dE_dAns
+    dE_dw2 = np.dot(hid.T, dE_dAns)
+    dE_db2 = dE_dAns
 
-dE_dHid = np.dot(dE_dAns, w2.T) * tanh_derivative(tensor_1)
-dE_dw1 = np.dot(x.T, dE_dHid)
-dE_db1 = dE_dHid
+    dE_dHid = np.dot(dE_dAns, w2.T) * tanh_derivative(tensor_1)
+    dE_dw1 = np.dot(x.T, dE_dHid)
+    dE_db1 = dE_dHid
 
-# Correction
-w1 -= ALPHA * dE_dw1
-b1 -= ALPHA * dE_db1
-w2 -= ALPHA * dE_dw2
-b2 -= ALPHA * dE_db2
+    # Correction
+    w2 += ALPHA * dE_dw2
+    b2 += ALPHA * dE_db2
 
-print(e)
+    w1 += ALPHA * dE_dw1
+    b1 += ALPHA * dE_db1
+
+
+
+
+    print(ans * y)
+
